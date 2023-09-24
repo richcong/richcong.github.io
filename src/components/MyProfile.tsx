ca61f48aa0ea773d721e18a7ba643bf2a4360568
richcong <54823716+richcong@users.noreply.github.com>
2023-09-24 17:45:30 +1000

    feat: header scroll

--- src/components/MyProfile.tsx
+++ src/components/MyProfile.tsx
@@ -26,7 +26,19 @@ import EditRoundedIcon from "@mui/icons-material/EditRounded";
 import EmailRoundedIcon from "@mui/icons-material/EmailRounded";
 import { Chip, Tooltip } from "@mui/joy";
 
-export default function MyProfile() {
+type MyProfileProps = {
+  bioRef: React.RefObject<HTMLDivElement>;
+  resumeRef: React.RefObject<HTMLDivElement>;
+  projectsRef: React.RefObject<HTMLDivElement>;
+  hobbiesRef: React.RefObject<HTMLDivElement>;
+};
+
+export default function MyProfile({
+  bioRef,
+  resumeRef,
+  projectsRef,
+  hobbiesRef,
+}: MyProfileProps) {
   return (
     <Box
       sx={{
@@ -50,7 +62,7 @@ export default function MyProfile() {
           },
         }}
       >
-        <Card>
+        <Card ref={bioRef}>
           <Box display="flex" flexDirection="row" sx={{ mb: 1 }}>
             <Box>
               <Typography level="title-md">Bio</Typography>
@@ -61,24 +73,26 @@ export default function MyProfile() {
             <Box flex={1}></Box>
             <Tooltip title="GitHub">
               <IconButton
+                component="a"
+                href="https://github.com/richcong"
+                target="_blank"
+                rel="noopener noreferrer"
                 sx={{
                   "--IconButton-size": "40px",
                 }}
-                onClick={() =>
-                  window.open("https://github.com/richcong", "_blank")
-                }
               >
                 <GitHubIcon />
               </IconButton>
             </Tooltip>
             <Tooltip title="LinkedIn">
               <IconButton
+                component="a"
+                href="https://linkedin.com/in/richardcong"
+                target="_blank"
+                rel="noopener noreferrer"
                 sx={{
                   "--IconButton-size": "40px",
                 }}
-                onClick={() =>
-                  window.open("https://linkedin.com/in/richardcong", "_blank")
-                }
               >
                 <LinkedInIcon />
               </IconButton>
@@ -124,45 +138,55 @@ export default function MyProfile() {
                 <Stack direction="row" spacing={1}>
                   <Chip
                     startDecorator={<FaReact />}
-                    onClick={() => window.open("https://react.dev/", "_blank")}
+                    component="a" //shows link on bottom of page on hover
+                    href="https://react.dev/"
+                    target="_blank" //opens in new tab
+                    rel="noopener noreferrer" //adds security when opening new link
+                    onClick={() => null} //adds onHover effect
                     size="md"
                   >
                     React
                   </Chip>
                   <Chip
                     startDecorator={<SiJavascript />}
-                    onClick={() =>
-                      window.open(
-                        "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/What_is_JavaScript",
-                        "_blank"
-                      )
-                    }
+                    component="a"
+                    href="https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/What_is_JavaScript"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    onClick={() => null}
                     size="md"
                   >
                     Javascript
                   </Chip>
                   <Chip
                     startDecorator={<SiMui />}
-                    onClick={() => window.open("https://mui.com/", "_blank")}
+                    component="a"
+                    href="https://mui.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    onClick={() => null}
                     size="md"
                   >
                     Material UI
                   </Chip>
                   <Chip
                     startDecorator={<BiLogoSpringBoot />}
-                    onClick={() => window.open("https://spring.io/", "_blank")}
+                    component="a"
+                    href="https://spring.io/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    onClick={() => null}
                     size="md"
                   >
                     Java Spring
                   </Chip>
                   <Chip
                     startDecorator={<GrOracle />}
-                    onClick={() =>
-                      window.open(
-                        "https://docs.oracle.com/en/database/oracle/oracle-database/19/sqlrf/Introduction-to-Oracle-SQL.html#GUID-049B7AE8-11E1-4110-B3E4-D117907D77AC",
-                        "_blank"
-                      )
-                    }
+                    component="a"
+                    href="https://docs.oracle.com/en/database/oracle/oracle-database/19/sqlrf/Introduction-to-Oracle-SQL.html#GUID-049B7AE8-11E1-4110-B3E4-D117907D77AC"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    onClick={() => null}
                     size="md"
                   >
                     Oracle SQL
@@ -269,7 +293,7 @@ export default function MyProfile() {
             </CardActions>
           </CardOverflow>
         </Card>
-        <Card>
+        <Card ref={resumeRef}>
           <Box sx={{ mb: 1 }}>
             <Typography level="title-md">Resume</Typography>
 
@@ -300,9 +324,30 @@ export default function MyProfile() {
             </CardActions>
           </CardOverflow>
         </Card>
-        <Card>
+        <Card ref={projectsRef}>
           <Box sx={{ mb: 1 }}>
-            <Typography level="title-md">Portfolio projects</Typography>
+            <Typography level="title-md">Projects</Typography>
+            <Typography level="body-sm">
+              Share a few snippets of your work.
+            </Typography>
+          </Box>
+
+          <Divider />
+          <Stack spacing={2} sx={{ my: 1 }}></Stack>
+          <CardOverflow sx={{ borderTop: "1px solid", borderColor: "divider" }}>
+            <CardActions sx={{ alignSelf: "flex-end", pt: 2 }}>
+              <Button size="sm" variant="outlined" color="neutral">
+                Cancel
+              </Button>
+              <Button size="sm" variant="solid">
+                Save
+              </Button>
+            </CardActions>
+          </CardOverflow>
+        </Card>
+        <Card ref={hobbiesRef}>
+          <Box sx={{ mb: 1 }}>
+            <Typography level="title-md">Hobbies</Typography>
             <Typography level="body-sm">
               Share a few snippets of your work.
             </Typography>
