14a9ac61733ef8d7321fd62f505fa638bc8616ef
rcong-laptop <rcong@laptop>
2024-06-02 22:34:06 +1000

    feat: changed resume 2024

--- src/components/ResumeTimeline.tsx
+++ src/components/ResumeTimeline.tsx
@@ -1,6 +1,4 @@
 import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
-import ConstructionIcon from "@mui/icons-material/Construction";
-import SchoolIcon from "@mui/icons-material/School";
 import Typography from "@mui/joy/Typography";
 import Timeline from "@mui/lab/Timeline";
 import TimelineConnector from "@mui/lab/TimelineConnector";
@@ -12,6 +10,7 @@ import TimelineOppositeContent, {
 } from "@mui/lab/TimelineOppositeContent";
 import TimelineSeparator from "@mui/lab/TimelineSeparator";
 import "../css/common/List.css";
+import BusinessIcon from "@mui/icons-material/Business";
 
 export default function ResumeTimeline() {
   return (
@@ -28,10 +27,32 @@ export default function ResumeTimeline() {
           variant="body2"
           color="text.secondary"
         >
-          2023
+          2024
         </TimelineOppositeContent>
         <TimelineSeparator>
-          <TimelineConnector sx={{ bgcolor: "primary.main" }} />
+          <TimelineConnector />
+          <TimelineDot color="primary">
+            <BusinessIcon />
+          </TimelineDot>
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent sx={{ py: "12px", px: 2 }}>
+          <Typography level="title-md" component="span">
+            Leidos
+          </Typography>
+          <Typography>Software Engineer</Typography>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent
+          sx={{ m: "auto 0" }}
+          variant="body2"
+          color="text.secondary"
+        >
+          2021
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineConnector />
           <TimelineDot color="primary">
             <AccountBalanceIcon />
           </TimelineDot>
@@ -43,50 +64,6 @@ export default function ResumeTimeline() {
           </Typography>
           <Typography>Software Developer</Typography>
           <Typography>Software Tester</Typography>
-        </TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineOppositeContent
-          sx={{ m: "auto 0" }}
-          variant="body2"
-          color="text.secondary"
-        >
-          2022
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineConnector />
-          <TimelineDot color="primary" variant="outlined">
-            <SchoolIcon />
-          </TimelineDot>
-        </TimelineSeparator>
-        <TimelineContent sx={{ py: "12px", px: 2 }}>
-          <Typography level="title-md" component="span">
-            Australian National University
-          </Typography>
-          <Typography>Bachelor of IT</Typography>
-        </TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineOppositeContent
-          sx={{ m: "auto 0" }}
-          align="right"
-          variant="body2"
-          color="text.secondary"
-        >
-          2020
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineConnector />
-          <TimelineDot>
-            <ConstructionIcon />
-          </TimelineDot>
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent sx={{ py: "12px", px: 2 }}>
-          <Typography level="title-md" component="span">
-            Deloitte
-          </Typography>
-          <Typography>Platform Engineer Intern</Typography>
         </TimelineContent>
       </TimelineItem>
     </Timeline>
